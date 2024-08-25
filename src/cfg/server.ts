6a9c94407f84eab3741e514190b54c4305ad8514
CoZmoTheGod <37917612+CoZmoTheGod@users.noreply.github.com>
2024-08-25 12:35:22 -0500

    Update server.ts

--- src/cfg/server.ts
+++ src/cfg/server.ts
@@ -1,10 +1,18 @@
-import { IServerCfg } from "../interfaces";
+import { IServerCfg, IServerItem } from "../interfaces";
+
+// this is actually required for the response to be parsed correctly by the client...
+const mandatoryPart: IServerItem = {
+  ipParts: [72, 9, 153, 235],
+  port: 25888,
+  full: false,
+  empty: false,
+};
 
 export const serverCfg: IServerCfg = {
-    game: "H2M", // was H1
-    protocol: 2,
-    udpPort: 20810,
-    listFetchTime: 1000 * 60 * 5,
-    raidmaxApiUrl: "http://api.raidmax.org:5000/servers",
-    mandatoryServerParts: [], // removed the mandatory part
+  game: "H2M", // was H1
+  protocol: 2,
+  udpPort: 20810,
+  listFetchTime: 1000 * 60 * 5,
+  raidmaxApiUrl: "http://api.raidmax.org:5000/servers",
+  mandatoryServerParts: [], // removed the mandatory part
 };
